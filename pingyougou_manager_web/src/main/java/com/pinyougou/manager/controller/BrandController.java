49f20c1735cf894eae79525f8f57e3e31a3bf8ca
zeng <369925837@qq.com>
2019-04-11 19:54:37 +0800

    brand 分页完成

--- pingyougou_manager_web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pingyougou_manager_web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -3,6 +3,7 @@ package com.pinyougou.manager.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import entity.PageResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +16,24 @@ public class BrandController {
     @Reference
     private BrandService brandService;
 
-
+    /**
+     * 返回全部列表
+     * @return
+     */
     @RequestMapping("/findAll")
     public List<TbBrand> findAll(){
         return brandService.findAll();
+    }
+
+    /**
+     * 返回全部列表 带分页
+     * @param page
+     * @param size
+     * @return
+     */
+    @RequestMapping("/findPage")
+    public PageResult findPage(int page,int size){
+        return brandService.findPage(page,size);
     }
 
 }
--- pingyougou_sellergoods_interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pingyougou_sellergoods_interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -1,9 +1,18 @@
 package com.pinyougou.sellergoods.service;
 
 import com.pinyougou.pojo.TbBrand;
+import entity.PageResult;
 
 import java.util.List;
 
 public interface BrandService {
+    /**
+     * 返回所有列表
+     * @return
+     */
     public List<TbBrand> findAll();
+
+    public PageResult findPage(int pageNum,int PageSize);
+
+
 }
