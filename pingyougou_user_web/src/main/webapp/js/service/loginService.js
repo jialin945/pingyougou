d2825299f8bdba69fad0b3063184e0420c90b5eb
zeng <369925837@qq.com>
2019-04-30 18:44:42 +0800

    第15天  11111111

--- pingyougou_user_web/src/main/webapp/js/service/loginService.js
+++ pingyougou_user_web/src/main/webapp/js/service/loginService.js
@@ -1,6 +1,7 @@
 app.service("loginService", function ($http) {
     this.showName=function () {
         //读取列表数据绑定到表单中
+        //return $http.get("../login/name.do");  //都可以
         return $http.get("login/name.do");
     }
 });
